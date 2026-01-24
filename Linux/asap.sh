a821fd35d833ea015970893099e88583055c58ee
Blake Bedford <bake0101@proton.me>
2026-01-23 22:51:47 -0800

    Create asap.sh

---
+++ Linux/asap.sh
@@ -0,0 +1,7 @@
+sh inventory/inventory.sh
+sh hardenScripts/ssh.sh
+sh hardenScripts/key_perms.sh
+sh utils/LSMS_Setup.sh
+sh utils/LSMS_monitor.sh
+
+echo "Paste the output in Discord immediately!!"
