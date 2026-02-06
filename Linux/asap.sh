73e04a2fc3f2ae414cbd3a73bf86ccce12a461bc
Blake Bedford <bake0101@proton.me>
2026-02-05 19:21:15 -0800

    major glowup

--- Linux/asap.sh
+++ Linux/asap.sh
@@ -1,7 +1,20 @@
-sh inventory/inventory.sh
-sh hardenScripts/ssh.sh
-sh hardenScripts/key_perms.sh
-sh utils/LSMS_Setup.sh
-sh utils/LSMS_monitor.sh
+sh "$(dirname "$0")/utils/changePassword.sh" "root"
+sudo passwd -l root > /dev/null
 
-echo "Paste the output in Discord immediately!!"
+USER=graypcf
+PASS=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 20)
+OUTPUT=$(PASS="$PASS" sh "$(dirname "$0")/utils/createSudoUser.sh" "$USER" 2>&1)
+
+if echo "$OUTPUT" | grep -q "already exists"; then
+    echo "$OUTPUT"
+else
+    echo "$USER,$PASS"
+fi
+
+sh "$(dirname "$0")/hardenScripts/ssh.sh"
+sh "$(dirname "$0")/hardenScripts/key_perms.sh"
+sh "$(dirname "$0")/utils/LSMS_Setup.sh"
+sh "$(dirname "$0")/utils/LSMS_monitor.sh"
+sh "$(dirname "$0")/inventory/inventory.sh"
+
+echo "Paste inventory to Discord"
