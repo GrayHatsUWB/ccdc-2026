134d6177b792720c18577a2aa4dfd0d6c56cb33b
Blake Bedford <bake0101@proton.me>
2026-02-05 19:25:42 -0800

    ok i dont even have an excuse for how bad i was twekaing holy shit

--- Linux/asap.sh
+++ Linux/asap.sh
@@ -2,12 +2,10 @@ sh "$(dirname "$0")/utils/changePassword.sh" "root"
 sudo passwd -l root > /dev/null
 
 USER=graypcf
-PASS="$PASS" sh "$(dirname "$0")/utils/createSudoUser.sh" "$USER" 2>&1
+sh "$(dirname "$0")/utils/createSudoUser.sh" "$USER" 2>&1
 
 sh "$(dirname "$0")/hardenScripts/ssh.sh"
 sh "$(dirname "$0")/hardenScripts/key_perms.sh"
 sh "$(dirname "$0")/utils/LSMS_Setup.sh"
 sh "$(dirname "$0")/utils/LSMS_monitor.sh"
 sh "$(dirname "$0")/inventory/inventory.sh"
-
-echo "Paste inventory to Discord"
