95195720b4d1073517c351bff15c8167196ccf82
Blake Bedford <bake0101@proton.me>
2026-02-05 19:23:14 -0800

    ok i said major glow up but i was so much of a visionary that i saw the future but unfortunately not the present. so now it had a glow up.

--- Linux/asap.sh
+++ Linux/asap.sh
@@ -2,14 +2,7 @@ sh "$(dirname "$0")/utils/changePassword.sh" "root"
 sudo passwd -l root > /dev/null
 
 USER=graypcf
-PASS=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 20)
-OUTPUT=$(PASS="$PASS" sh "$(dirname "$0")/utils/createSudoUser.sh" "$USER" 2>&1)
-
-if echo "$OUTPUT" | grep -q "already exists"; then
-    echo "$OUTPUT"
-else
-    echo "$USER,$PASS"
-fi
+PASS="$PASS" sh "$(dirname "$0")/utils/createSudoUser.sh" "$USER" 2>&1
 
 sh "$(dirname "$0")/hardenScripts/ssh.sh"
 sh "$(dirname "$0")/hardenScripts/key_perms.sh"
